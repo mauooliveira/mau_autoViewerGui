ab5c50c57cf654a09a91eb794814f4d3fbba3560
Mau Oliveira <96890754+mauooliveira@users.noreply.github.com>
2021-12-30 22:21:33 +0000

    Create mau_autoViewerGui.py
    # version: 1.0.0
    # last updated: 30.05.21

---
+++ mau_autoViewerGui.py
@@ -0,0 +1,33 @@
+#--------------------------------------------------
+# mau_autoViewerGui.py
+# version: 1.0.0
+# last updated: 30.05.21
+#--------------------------------------------------
+
+import nuke
+
+def autoViewerGui():
+    #FOR EACH SELECTED NODE
+    for i in nuke.selectedNodes():
+        #EVALUATE CLASS
+        if i.Class() == "Switch":
+            #BEING A SWITCH SET/REMOVE EXPRESSION ON THE WHICH KNOB
+            a = i.knob("which").hasExpression()
+            if a == True:
+                k = i.knob("which")
+                k.setAnimated()
+                k.setValue(0)
+                k.clearAnimated()
+            else:
+                i.knob("which").setExpression("$gui")
+        #IF IS A VIEWER JUST PASS        
+        elif i.Class() == "Viewer":
+            pass
+        #IF ANY OTHER TYPE OF NODE SET/REMOVE EXPRESSION TO THE DISABLE KNOB
+        else:
+            a = i.knob("disable").isAnimated()
+            if a == True:
+                i.knob("disable").clearAnimated()
+                i.knob("disable").setValue(False)
+            else:
+                i.knob("disable").setExpression("$gui")
